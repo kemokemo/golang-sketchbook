97df64871b6de2111be265375f4f886ecc35a79f
kemokemo <t2wonderland@gmail.com>
2020-08-30 18:19:50 +0900

    feat: add semaphone for crawler.

--- gopl_io/crawl2/main.go
+++ gopl_io/crawl2/main.go
@@ -8,13 +8,18 @@ import (
 
 func main() {
 	worklist := make(chan []string)
+	var n int // the number of waiting to send worklist
+
+	n++
 	go func() { worklist <- os.Args[1:] }()
 
 	seen := make(map[string]bool)
-	for list := range worklist {
+	for ; n > 0; n-- {
+		list := <-worklist
 		for _, link := range list {
 			if !seen[link] {
 				seen[link] = true
+				n++
 				go func(link string) {
 					worklist <- crawl(link)
 				}(link)
@@ -23,9 +28,16 @@ func main() {
 	}
 }
 
+// this tokens limits the number of parallel crawler.
+var tokens = make(chan struct{}, 20)
+
 func crawl(url string) []string {
 	fmt.Println(url)
+
+	tokens <- struct{}{} // get token
 	list, err := Extract(url)
+	<-tokens // relese token
+
 	if err != nil {
 		log.Printf("failed to extract url '%s': %v", url, err)
 	}
